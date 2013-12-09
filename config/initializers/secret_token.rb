c74567e0905a1bdb3e1308c18c531d0456606b6d
Christina Thompson <c.thompsonms@gmail.com>
2013-12-09 14:08:21 -0800

    Created Scamazon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scamazon::Application.config.secret_key_base = '38597a8bf93b57891fbb66bff915a85e6dfb6a232bfebaa618f151ec75719403a1efd657f656a1ac9f5550250cb3345e5b57fa75a8fbd61fb53fd663a2b6090a'
