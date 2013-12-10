1149cef94739d389870040b4762a0a02ee836a4b
Davida Marion <davidamarion@gmail.com>
2013-12-10 12:02:55 -0800

    products form works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,32 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Your product has been successfully saved!"
+    end
+  end
+
+  def show
+  end
+
+private
+
+  def set_product
+    @product=Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category, :price)
   end
   
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -6,9 +6,45 @@ describe ProductsController do
 
     it "is successful" do
       get :index
-      response.should be_successful
+      expect(response).to be_successful
     end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all the products" do
+      product1, product2 = Product.create!(name: "a", description: "b",category: "c", price: "100"), Product.create!(name: "b", description: "c",category: "d", price: "100")
+        get :index
+        expect(assigns(:products)).to match_array([product1, product2])
+    end
+  
   end
 
+
+  describe "GET 'new'" do
+
+    it "is successful" do
+      get :new
+      expect(response).to be_successful
+    end
+
+    it "renders the new product template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  
+  end
+ 
+ describe "POST 'index" do
+    
+    it "is successful" do
+      post :index
+      expect(response).to be_successful
+    end
+  
+  end
+
+  
 end
