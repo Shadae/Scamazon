a7ab5298fc88ed9a8dad759339cdbfb28d973639
Christina Thompson <c.thompsonms@gmail.com>
2013-12-09 17:08:54 -0800

    Wrote specs for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Scamazon::Application.routes.draw do
+
+  get "/reviews/new" => "reviews#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20131210003644_add_rating_categories_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddRatingCategoriesToReviews < ActiveRecord::Migration
+  def change
+    add_column :reviews, :rating, :integer
+  end
+end
