b954f7a66951e9c5b7021a2f35ab4ad107f3acdf
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-12-10 14:06:35 -0800

    changes to user's section

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210001333) do
+ActiveRecord::Schema.define(version: 20131210172236) do
 
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "user_name"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,8 @@ describe User do
   describe "validations" do
 
     let(:new_user) { User.new(email:     "foo@example.com",
-                              user_name: "Foo McBar") }
+                              user_name: "Foo McBar",
+                              password_digest:  "fluffybunnies") }
 
     it "must have an email" do
       new_user.email = nil
@@ -13,10 +14,12 @@ describe User do
     end
 
     it "must have a user name" do
+      new_user.user_name = nil
       expect(new_user).to be_invalid
     end
 
     it "must have a password" do
+      new_user.password_digest = nil
       expect(new_user).to be_invalid
     end
   end
