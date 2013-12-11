1ca1bfe1d6dac3b6c899440a0806a8b452a29452
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2013-12-11 13:56:29 -0800

    merging with purchasepages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210224218) do
+ActiveRecord::Schema.define(version: 20131211214033) do
+
+  create_table "order_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
