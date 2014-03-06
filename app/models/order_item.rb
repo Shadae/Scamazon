2213ccf26198031354bd432be0e0cac0134c545a
liz rush <liz@lizmrush.com>
2014-03-06 14:49:39 -0800

    returns model method for order items so they can be added to cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,11 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
+  def add(amount)
+    self.update(quantity: (quantity + amount.to_i))
+  end
+
+  def subtract(amount)
+    self.update(quantity: (quantity - amount.to_i))
+  end
 end
